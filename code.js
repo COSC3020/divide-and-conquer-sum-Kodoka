845b39633d87a78502c987b0a31a6d7011b91bfd
Kodoka <10721764+Kodoka@users.noreply.github.com>
2025-02-09 13:32:28 -0700

    Committing for funsies.

--- code.js
+++ code.js
@@ -1,3 +1,18 @@
-function divideAndConquerSum(a) {
-    return a;
+function divideAndConquerSum(a)
+{
+    var sum = 0;
+    divide(a, 0, a.length - 1, sum)
+}
+
+function divide(arrayToSummarize, low, high, sum)
+{
+    if(low >= high)
+    {
+        return;
+    }
+    var firstThird = Math.floor((low - high) / 3);
+    var secondThird = Math.floor(((high - high) / 3) * 2);
+    divide(arrayToSummarize, low, firstThird, sum);
+    divide(arrayToSummarize, firstThird + 1, secondThird, sum);
+    divide(arrayToSummarize, secondThird + 1, high, sum);
 }
