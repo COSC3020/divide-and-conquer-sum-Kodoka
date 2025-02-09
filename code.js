b3af54c58f670196b4996ea150da5ea0d75771f2
Kodoka <10721764+Kodoka@users.noreply.github.com>
2025-02-09 15:09:53 -0700

    Functional for two sub-arrays.

--- code.js
+++ code.js
@@ -1,18 +1,31 @@
+// Delete when done testing.
+var testArr = [1, 2, 3, 4, 5, 6];
+console.log(divideAndConquerSum(testArr));
+
+// Returns the sum of the elements of an array.
 function divideAndConquerSum(a)
 {
-    var sum = 0;
-    divide(a, 0, a.length - 1, sum)
+    return divideArray(a, 0, a.length - 1)
 }
 
-function divide(arrayToSummarize, low, high, sum)
+// Recursively divides an array down to a single element, then returns it.
+function divideArray(arrayToSummarize, low, high)
 {
-    if(low >= high)
+    // If our low and high indices ever invert, just return, there's nothing to see in these forbidden regions.
+    if(low > high)
     {
         return;
     }
-    var firstThird = Math.floor((low - high) / 3);
-    var secondThird = Math.floor(((high - high) / 3) * 2);
-    divide(arrayToSummarize, low, firstThird, sum);
-    divide(arrayToSummarize, firstThird + 1, secondThird, sum);
-    divide(arrayToSummarize, secondThird + 1, high, sum);
+    // If the region of the array we're looking at is a single element, the base case, then we'll return that element.
+    else if(low === high)
+    {
+        return arrayToSummarize[low];
+    }
+    // Else, if the region is larger than a single element, we'll continue recursively calling on smaller regions.
+    else if(low < high)
+    {
+        var mid = Math.floor((low + high) / 2);
+        return divideArray(arrayToSummarize, low, mid) +
+            divideArray(arrayToSummarize, mid + 1, high);
+    }
 }
